1c5cde2100bafe09d38a5fdb96c666946da90236
Matthew Johnson <mjohnson082396@utexas.edu>
2018-09-17 17:21:33 -0500

    fixed bugs with vision.py not formatting attributes into .csv

--- vision.py
+++ vision.py
@@ -1,5 +1,6 @@
 import io
 import glob, os
+import csv
 from google.cloud import vision
 from google.cloud.vision import types
 
@@ -10,7 +11,11 @@ descriptions of labels
 def get_labels(img):
 	# label detection
 	response = vision_client.label_detection(image=img)
-	return response.label_annotations	
+	labels = response.label_annotations	
+	descriptions = '"'
+	for label in labels:
+		descriptions+= label.description + ', '
+	return descriptions + '"'
 
 '''
 I'll just grab the images from my machine. I'm not going to use the Box API
@@ -22,26 +27,37 @@ since they're stored locally on my machine like:
 		...
 	...
 
-To make it easy on myself, I'll create a dictionary of the form <image_name: <list_of_labels>>
-then just format the dictionary into a .csv
-
 '''
-label_dict = {}
+images_list = []
 vision_client = vision.ImageAnnotatorClient()
 
+# get all file paths in the directory
 for dir, subdir, files in os.walk('/Users/mattjohnson/Desktop/Research/Images'):
 	for file in files:
 		file_name = os.path.join(dir, file)
-		if ('.jpg' not in file_name or os.path.getsize(file_name) == 0):
+		if ('.jpg' not in file_name or os.path.getsize(file_name) == 0 or '.csv' in file_name):
 			continue
 		print file_name
 		with io.open(file_name, 'rb') as image_file:
 			content = image_file.read()
-		image = types.Image(content=content)
+		try:
+			image = types.Image(content=content)
+		except: 	
+			print "ERROR WITH " + file_name
+			# write to .csv...
+			with open('google_cloud_image_annotations.csv', 'w') as f:
+				mywriter = csv.writer(f)
+				mywriter.writerow(['image', 'tags'])
+				for d in images_list:
+					mywriter.writerow(d)
 
-		# create dict of the form <image_name: <list o' labels>>
-		label_dict['file_name'] = get_labels(image)
+
+		# create list of of the form [image_name, string of labels] to resemble one image>
+		images_list.append([file_name,get_labels(image)])
 
 # write to .csv...
 with open('google_cloud_image_annotations.csv', 'w') as f:
-    [f.write('{0},{1}\n'.format(key, value)) for key, value in label_dict.items()]
+	mywriter = csv.writer(f)
+	mywriter.writerow(['image', 'tags'])
+	for d in images_list:
+		mywriter.writerow(d)
