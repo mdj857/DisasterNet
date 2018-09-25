68f76ba178431eb2bbdf8c0babff7e628b211b89
Matthew Johnson <mjohnson082396@utexas.edu>
2018-09-25 14:09:44 -0500

    plots completed

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from collections import Counter
 import sys
+
 
 '''
 This function plots the top n most frequent tags for a given date. 
@@ -28,22 +31,23 @@ def plot_frequency_of_top_n_words(date, n):
     
 	plt.bar(x_pos, count,align='center')
 	plt.xticks(x_pos, tags, rotation = 'vertical') 
-	
-	plt.grid()
+	plt.xlabel('Tags')
+	plt.ylabel('Frequency')
+	plt.title('Tag Frequency for ' + date)
 
 	plt.gca().margins(x=0)
 	plt.gcf().canvas.draw()
 	tl = plt.gca().get_xticklabels()
+	plt.draw()
 	maxsize = max([t.get_window_extent().width for t in tl])
-	m = 0.2 # inch margin
-	s = maxsize/plt.gcf().dpi*N+2*m
+	m = 0.5 # inch margin
+	s = maxsize/plt.gcf().dpi*len(tags)+2*m
 	margin = m/plt.gcf().get_size_inches()[0]
 
-	plt.gcf().subplots_adjust(left=margin, right=1.-margin)
+	plt.gcf().subplots_adjust(left=margin, right=1.-margin, bottom=0.1)
 	plt.gcf().set_size_inches(s, plt.gcf().get_size_inches()[1])
-	plt.ylabel('Frequency')
+	plt.tight_layout()
 	plt.savefig(date+ str('_freq_analysis')+".png")
-	plt.show()
 
 
 	
