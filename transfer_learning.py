f286624002b17f528ce1b8c5ab4125c5184a4c1d
mdj857 <mjohnson082396@utexas.edu>
2018-10-16 15:10:09 -0500

    finished transfer_learning.py

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,31 +1,27 @@
 
-# coding: utf-8
-
-# In[3]:
-
 # This file uses transfer learning via the VGG16 convnet to build a model to identify 
 # the urgency of each images as coded by Brett
 
 import pickle
 import numpy as np
 from keras.models import Sequential
-from scipy.misc import imread
 import matplotlib.pyplot as plt
 import numpy as np
 import keras
 from keras.layers import Dense
 import pandas as pd
-
+from sklearn.model_selection import GridSearchCV
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 from keras.applications.vgg16 import decode_predictions
-from scipy.misc import imresize
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.layers import Dense, Activation
 
-with open('feat_vecs.pkl', 'rb') as f:
+with open('../feat_vecs.pkl', 'rb') as f:
 	feature_vecs_old= pickle.load(f)
-with open('Hurricane_Harvey/y_urgency.pkl', 'rb') as f:
+with open('../y_urgency.pkl', 'rb') as f:
 	Y_urgency_old = pickle.load(f)
 
 
@@ -71,50 +67,35 @@ Y=pd.get_dummies(Y)
 from sklearn.model_selection import train_test_split
 X_train, X_valid, Y_train, Y_valid=train_test_split(X,Y,test_size=0.2, random_state=42)
 
-print X_train.shape
-print Y_train.shape
-print type(X_valid)
 
 
-# In[ ]:
+X_valid = np.asarray(X_valid)
+Y_valid = np.asarray(Y_valid)
 
-# creating a mlp model
-from keras.layers import Dense, Activation
 model=Sequential()
 
-model.add(Dense(10000, input_dim=25088, activation='relu',kernel_initializer='uniform'))
-keras.layers.core.Dropout(0.4, noise_shape=None, seed=None)
-
-model.add(Dense(5000,input_dim=10000,activation='sigmoid'))
+model.add(Dense(1000, input_dim=25088, activation='relu',kernel_initializer='uniform'))
 keras.layers.core.Dropout(0.3, noise_shape=None, seed=None)
 
-model.add(Dense(1000,input_dim=5000,activation='sigmoid'))
-keras.layers.core.Dropout(0.3, noise_shape=None, seed=None)
-
-model.add(Dense(750,input_dim=1000,activation='sigmoid'))
-keras.layers.core.Dropout(0.3, noise_shape=None, seed=None)
-
-model.add(Dense(300,input_dim=750,activation='sigmoid'))
+model.add(Dense(500,input_dim=1000,activation='sigmoid'))
 keras.layers.core.Dropout(0.4, noise_shape=None, seed=None)
 
 model.add(Dense(150,input_dim=500,activation='sigmoid'))
 keras.layers.core.Dropout(0.2, noise_shape=None, seed=None)
 
-# number of output classification categories
 model.add(Dense(units=5))
 model.add(Activation('softmax'))
 
+
 model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
 
-# fitting the model 
-history = model.fit(np.asarray(X_train), np.asarray(Y_train), epochs=5,
-          batch_size=32)
+model.fit(X_train, Y_train, epochs=20, batch_size=128,validation_data=(X_valid,Y_valid))
 
+# serialize model to JSON
+model_json = model.to_json()
+with open("urgency_predictor_model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("urgency_predictor_weights.h5")
 
-# In[32]:
-
-X_valid = np.asarray(X_valid)
-Y_valid = np.asarray(Y_valid)
-
-model.test_on_batch(X_valid, Y_valid)
 
