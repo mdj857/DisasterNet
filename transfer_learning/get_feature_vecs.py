2fd9b716ab5fc960765f140e511cc8e872608795
mdj857 <mjohnson082396@utexas.edu>
2018-10-19 14:57:25 -0500

    fixed get_feat_vecs to only use one coder .csv

--- transfer_learning/get_feature_vecs.py
+++ transfer_learning/get_feature_vecs.py
@@ -4,38 +4,48 @@
 
 import pandas as pd
 from datetime import datetime  
-from datetime import timedelta 
+from datetime import timedelta
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
+import numpy as np
 
 images_path = 'coded_images/'
 # combine the .csvs into a single dataframe
-coder1 = pd.read_csv('Coder 1 CSV File.csv')
-coder2 = pd.read_csv('Coder 2 CSV File.csv')
-combined = coder1.append(coder2) 
+combined = pd.read_csv('Coder 1 CSV File.csv')
 
+print combined.head
 # function that gets the date given an offset that's been coded
 def get_date(coded_date):
 	date = datetime(2017,8,16) + timedelta(days=coded_date)
-	date = date.strftime('X%m-X%d-%Y').replace('X0','X').replace('X','')
+	date = date.strftime('X%m-X%d-%y').replace('X0','X').replace('X','')
 	return date
 
 def get_image_name(image_num):
 	return 'Image' + str(image_num) + '.jpg'
 
 def get_relative_file_path(date_num, im_num):
-	return get_date(date_num)
-
-
-#preparing the dataset -- assembling the images and their relevant labels into an x and y
-df.apply (lambda row: label_race (row),axis=1)
+	return get_date(date_num) + '/' + get_image_name(im_num)
 
 
 x_img=[]
-for index, row in combined.iterrows():
-	image_number = row['Q3ImageNumber']
-    temp_img=image.load_img(images_path+,target_size=(224,224))
-    temp_img=image.img_to_array(temp_img)
-    train_img.append(temp_img)
 
+
+for index, row in combined.iterrows():
+	i_num = row['Q3ImageNumber']
+	if i_num == ' ':
+		continue
+	date = row['Q1DateofImage']
+	
+	try: 
+		temp_img = image.load_img(images_path+get_relative_file_path(date, i_num), target_size=(224, 224))
+		temp_img = image.img_to_array(temp_img)
+		x_img.append(temp_img)
+		print "success for " + str(row)
+	except:
+		print "failed " + str(date) + "image num " + str(i_num) 
+ 
+		
 #converting train images to array and applying mean subtraction processing
 x_img=np.array(x_img) 
 train_img=preprocess_input(x_img)
@@ -48,4 +58,3 @@ model = VGG16(weights='imagenet', include_top=False)
 feat_vecs = model.predict(train_img)
 with open("feat_vecs.pkl", "wb") as fp:   #Pickling
 	pickle.dump(feat_vecs, fp)
-
